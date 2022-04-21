d62a9531b882e02e0b76f0c77d71bd4f895440eb
saisunkara77 <101804070+saisunkara77@users.noreply.github.com>
2022-04-21 16:52:25 +0100

    Create Employee.java

--- src/main/java/net/guides/springboot2/springboot2jpacrudexample/model/Employee.java
+++ src/main/java/net/guides/springboot2/springboot2jpacrudexample/model/Employee.java
@@ -6,7 +6,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
-
+// Vinay 
 @Entity
 @Table(name = "employees")
 public class Employee {
